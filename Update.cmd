c21ebe85fbed8068f7bf668e783c8e4d2aa52c15
unknown <acjparadis@outlook.com>
2022-07-21 02:42:22 -0600

    prepared for playtest

--- Update.cmd
+++ Update.cmd
@@ -1,7 +1,8 @@
 @echo off
-del /s /q WoLTib.zip
+for %%I in (.) do set CurrDirName=%%~nxI
+del /s /q %CurrDirName%.zip
 set zip="C:\Program Files\7-Zip\7z.exe"
-%zip% a -tzip WoLTib.zip *.SC2Map
-%zip% a -tzip WoLTib.zip WoLTibEdition.SC2Mod
-%zip% a -tzip WoLTib.zip metadata.txt
+%zip% a -mmt=8 -tzip %CurrDirName%.zip *.SC2Map
+%zip% a -mmt=8 -tzip %CurrDirName%.zip *.SC2Mod
+%zip% a -mmt=8 -tzip %CurrDirName%.zip metadata.txt
 pause
