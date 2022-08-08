d6efd18cd71f9a6135fcf86d46d1776f33f152c6
unknown <acjparadis@outlook.com>
2022-08-07 19:15:51 -0600

    symlink script for ccm is done

--- CCM.cmd
+++ CCM.cmd
@@ -1,17 +1,16 @@
 @echo off
-if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
-cd %~dp0
+net session >nul 2>&1
+if %errorlevel%==2 powershell -noprofile -command "&{ start-process '%~f0' -verb RunAs}" && exit
+cd /d %~dp0
 for /f "delims=" %%i in ('dir /b ^| find /V "SC2Mod"') do (
-	cd "C:\Program Files (x86)\StarCraft II\Maps\Campaign"
-	rem del /q %%i
-	rem mklink %%i %~dp0\%%i
-	echo %cd%\%%i %~dp0\%%i
+	cd /d "C:\Program Files (x86)\StarCraft II\Maps\Campaign"
+	del /q "%%i"
+	mklink "%%i" "%~dp0\%%i"
 )
-cd %~dp0
+cd /d %~dp0
 for /f "delims=" %%i in ('dir /b ^| find "SC2Mod"') do (
-	cd "C:\Program Files (x86)\StarCraft II\Mods"
-	rem rmdir /s /q "%%i"
-	rem mklink /d %%i "%~dp0\%%i"
-	echo %cd%\%%i %~dp0\%%i
+	cd /d "C:\Program Files (x86)\StarCraft II\Mods"
+	rmdir /s /q "%%i"
+	mklink /d "%%i" "%~dp0\%%i"
 )
 pause
