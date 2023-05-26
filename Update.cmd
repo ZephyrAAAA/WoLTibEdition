bbab4f46806b5c41c847ba313ffb4d69cc368b9d
unknown <acjparadis@outlook.com>
2023-05-26 17:00:10 -0600

    update the update script, increased version

--- Update.cmd
+++ Update.cmd
@@ -2,7 +2,7 @@
 for %%I in (.) do set CurrDirName=%%~nxI
 del /s /q %CurrDirName%.zip
 set zip="C:\Program Files\7-Zip\7z.exe"
-%zip% a -mmt=8 -tzip %CurrDirName%.zip *.SC2Map
-%zip% a -mmt=8 -tzip %CurrDirName%.zip *.SC2Mod
-%zip% a -mmt=8 -tzip %CurrDirName%.zip metadata.txt
+%zip% a -mmt=8 -tzip %CurrDirName%.zip *.SC2Map *.SC2Mod metadata.txt
+REM %zip% a -mmt=8 -tzip %CurrDirName%.zip 
+REM %zip% a -mmt=8 -tzip %CurrDirName%.zip 
 pause
